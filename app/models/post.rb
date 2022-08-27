c8d3958673c235e2897af7105adc98b3b1eab9ec
Fadel <fadel.g.heji@gmail.com>
2022-08-26 21:35:11 -0400

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :clickbait
+
+    def clickbait
+        unless title.present? && title.include?("Won't Believe" || "Secret" || "Top [\d]" || "Guess")
+            errors.add(:title, "Gotta work on your clickbaity title!")
+        end
+    end
 end
